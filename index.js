962fc896d516da14405d28ecd095c3b3307af5d3
Leandiv <leandro.divito@gmail.com>
2023-03-20 16:05:20 -0300

    Part 3 - Exercise 3.15: Changed delete request to use database instead

--- index.js
+++ index.js
@@ -48,15 +48,14 @@ app.get('/api/persons/:id', jsonParser,(request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    persons = persons.filter(person => person.id != id)
-    response.status(204).end()
-  } else {
-    response.status(404).end()
-  }
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
 })
 
 app.post('/api/persons', jsonParser, (request, response) => {
